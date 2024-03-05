fec0dfa53bb168eaca3fb8e1825b9148e6392c76
Deepraj Madhavrao Vadhwane <deeprajvadhwane01@gmail.com>
2024-03-05 22:11:37 +0530

    deployment

--- regex/webapp/app.py
+++ regex/webapp/app.py
@@ -13,9 +13,8 @@ def result():
     text_data = request.form['text_data']
     
     matches = re.findall(regex_exp, text_data)
-    num_matches = len(matches)
-    
-    return render_template('index.html', matches=matches,num_matches=num_matches)
+          
+    return render_template('index.html', matches=matches)
 
 if __name__ == '__main__':
     app.run(debug=True)
